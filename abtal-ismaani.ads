bab4cc3aa367dce0dd8914abbeee1e236f0eb92c
chaimalk <108950943+chaimalk@users.noreply.github.com>
2022-08-26 19:37:27 +0200

    Create abtal-ismaani.ads

---
+++ abtal-ismaani.ads
@@ -0,0 +1,55 @@
+{
+	"Playstore": {
+
+		"AdsController": {
+
+			"NetworkAds": "unity",
+
+			"BannerAdmob": "ca-app-pub-7448804041969308/4865537346",
+			"InterstitialAdmob": "ca-app-pub-7448804041969308/4004129231",
+
+			"BannerFacebook": "1147133002507840_1147133722507768",
+			"InterstitialFacebook": "1147133002507840_1147134875840986",
+			
+			"unityGameID": "4903215",
+			"UnityPlacement": "Interstitial_Android",
+
+			"StartappAds": "207711067",
+
+			"Cpa": false,
+			"ImageUrl": "https://i.imgur.com/oWEjQGy.gif",
+			"LinkCpa1": "https://cpbldi.com/911232e",
+			"LinkCpa2": "https://google.com/",
+
+			"watchNow": true,
+			"Rating": true
+
+		}
+
+	},
+	"Huaweistore": {
+
+		"AdsController": {
+
+			"NetworkAds": "startapp",
+
+			"BannerAdmob": "ca-app-pub-8082149907872048/3898325980",
+			"InterstitialAdmob": "ca-app-pub-8082149907872048/8781442418",
+
+			"BannerFacebook": "1347348165628536_1347349145628438",
+			"InterstitialFacebook": "1347348165628536_1347353132294706",
+
+			"StartappAds": "211203742",
+
+			"Cpa": false,
+			"ImageUrl": "https://i.imgur.com/oWEjQGy.gif",
+			"LinkCpa1": "https://cpbldi.com/911232e",
+			"LinkCpa2": "https://google.com/",
+
+			"watchNow": false,
+			"Rating": false
+
+		}
+
+	}
+}
